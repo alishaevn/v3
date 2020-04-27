16697f220677953bfde1c69a2457446b2f48d94e
alishaevn <alieva.inc@gmail.com>
2020-04-26 23:25:31 -0700

    about me section styling and wording

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 'use strict';
 
 const height = window.innerHeight;
+const width = window.innerWidth;
+
+const divider = document.querySelector('.divider');
 const home = document.querySelector('#home');
 
+divider.style.setProperty('--element-width', width);
 home.style.setProperty('--element-height', height);
