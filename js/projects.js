01cd53dffa0e9455fd80bf890e8c8b2843b33d6d
alishaevn <alieva.inc@gmail.com>
2020-04-28 23:55:14 -0700

    clicking the vizer project shows its modal

--- js/projects.js
+++ js/projects.js
@@ -1,30 +1,24 @@
 'use strict';
 
-const modalTriggers = document.querySelectorAll('.project-button');
-// const modalCloseTrigger = document.querySelector('.popup-modal__close');
+const modalOpenTriggers = document.querySelectorAll('.project-button');
 const bodyBlackout = document.querySelector('.blackout');
 
-// const closeModal = () => {
-//   popupModal.classList.remove('is--visible');
-//   bodyBlackout.classList.remove('is-blacked-out');
-// };
+modalOpenTriggers.forEach(trigger => {
+	trigger.addEventListener('click', () => {
+		const { projectButton } = trigger.dataset;
+		const popupModal = document.querySelector(`[data-project-modal='${projectButton}']`);
 
-modalTriggers.forEach(trigger => {
-  trigger.addEventListener('click', () => {
-    const { projectButton } = trigger.dataset;
-    const popupModal = document.querySelector(`[data-project-modal='${projectButton}']`);
-
-    popupModal.classList.add('is--visible');
-    bodyBlackout.classList.add('is-blacked-out');
+    	popupModal.classList.add('is--visible');
+    	bodyBlackout.classList.add('is-blacked-out');
     
-    // popupModal.querySelector('.popup-modal__close').addEventListener('click', () => {
-    //   popupModal.classList.remove('is--visible');
-    //   bodyBlackout.classList.remove('is-blacked-out');
-    // });
+    	popupModal.querySelector('.popup-modal__close').addEventListener('click', () => {
+    		popupModal.classList.remove('is--visible');
+    		bodyBlackout.classList.remove('is-blacked-out');
+    	});
     
-    bodyBlackout.addEventListener('click', () => {
-      popupModal.classList.remove('is--visible');
-      bodyBlackout.classList.remove('is-blacked-out');
-    });
-  });
+    	bodyBlackout.addEventListener('click', () => {
+    		popupModal.classList.remove('is--visible');
+    		bodyBlackout.classList.remove('is-blacked-out');
+    	});
+	});
 });
