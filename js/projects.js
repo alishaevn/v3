6098cb1079a47d75bfa757931108cdd5660c34b1
alishaevn <alieva.inc@gmail.com>
2020-05-01 23:42:07 -0700

    apply new eslint formatting

--- js/projects.js
+++ js/projects.js
@@ -1,33 +1,39 @@
 /* eslint-disable indent */
-'use strict';
+'use strict'
 
 const blackout = document.querySelector('.blackout');
 const body = document.querySelector('body');
 const modalOpenTriggers = document.querySelectorAll('.project-button');
 
+const blackout = document.querySelector('.blackout')
+const body = document.querySelector('body')
+const modalOpenTriggers = document.querySelectorAll('.project-button')
+
 modalOpenTriggers.forEach(trigger => {
 	trigger.addEventListener('click', () => {
+		const { projectButton } = trigger.dataset
+		const popupModal = document.querySelector(`[data-project-modal='${projectButton}']`)
 		const { projectButton } = trigger.dataset;
 		const popupModal = document.querySelector(`[data-project-modal='${projectButton}']`);
 
 		
-		popupModal.classList.add('is--visible');
-		blackout.classList.add('is-blacked-out');
-		body.classList.add('hidden');
+		popupModal.classList.add('is--visible')
+		blackout.classList.add('is-blacked-out')
+		body.classList.add('hidden')
 		
-		popupModal.scrollIntoView();
+		popupModal.scrollIntoView()
     
 		popupModal.querySelector('.popup-modal__close').addEventListener('click', () => {
-			popupModal.classList.remove('is--visible');
-			blackout.classList.remove('is-blacked-out');
-			body.classList.remove('hidden');
+			popupModal.classList.remove('is--visible')
+			blackout.classList.remove('is-blacked-out')
+			body.classList.remove('hidden')
 			
-		});
+		})
     
 		blackout.addEventListener('click', () => {
-			popupModal.classList.remove('is--visible');
-			blackout.classList.remove('is-blacked-out');
-			body.classList.remove('hidden');
-		});
-	});
-});
+			popupModal.classList.remove('is--visible')
+			blackout.classList.remove('is-blacked-out')
+			body.classList.remove('hidden')
+		})
+	})
+})
