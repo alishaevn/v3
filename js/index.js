efcbe63b10ee5a74296a177b0068a13ade7908e0
alishaevn <alieva.inc@gmail.com>
2020-05-02 00:15:29 -0700

    udpated to use eslint rules

--- js/index.js
+++ js/index.js
@@ -1,14 +1,13 @@
-/* eslint-disable indent */
-'use strict';
+'use strict'
 
-const height = window.innerHeight;
-const width = window.innerWidth;
+const height = window.innerHeight
+const width = window.innerWidth
 
-const dividers = document.querySelectorAll('.divider');
-const sections = document.querySelectorAll('section');
+const dividers = document.querySelectorAll('.divider')
+const sections = document.querySelectorAll('section')
 
-dividers.forEach(divider => divider.style.setProperty('--divider-width', width));
+dividers.forEach(divider => divider.style.setProperty('--divider-width', width))
 sections.forEach(section => {
-	section.style.setProperty('--section-height', height);
-	section.style.setProperty('--section-width', width);
-});
+	section.style.setProperty('--section-height', height)
+	section.style.setProperty('--section-width', width)
+})
